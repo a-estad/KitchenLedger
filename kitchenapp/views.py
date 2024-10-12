a66a21c3ad5fc3640e48ba1e273ef9cbab87f0d9
a-estad <ahestad@outlook.com>
2024-10-12 15:56:34 +0200

    feat(delete expense)
    sidebar is now only displayed if user is authenticated
    
    expenses can be deleted by a user if the user is the creator of the given expense

--- kitchenapp/views.py
+++ kitchenapp/views.py
@@ -17,22 +17,29 @@ def data_tables(request):
     all_expenses = Expense.objects.all()
     all_debts = Debt.objects.all()
 
-    if request.method == "POST":
-        form = ExpenseForm(request.POST)
+    if request.method == 'POST':
 
-        if form.is_valid():
-            expense_input = form.cleaned_data
-            expense = Expense(paid_by=request.user.resident,
-                              date=expense_input.get('date'),
-                              cost=expense_input.get('cost'),
-                              is_dinner_club=False,
-                              description=expense_input.get('description'))
-            expense.save()
+        if 'expense_id_delete' in request.POST:
+            expense_id_delete = request.POST.get('expense_id_delete')
+            Expense.objects.get(pk=expense_id_delete).delete()
+            form = ExpenseForm()
+
+        else:
+            form = ExpenseForm(request.POST)
+
+            if form.is_valid():
+                expense_input = form.cleaned_data
+                expense = Expense(paid_by=request.user.resident,
+                                  date=expense_input.get('date'),
+                                  cost=expense_input.get('cost'),
+                                  is_dinner_club=False,
+                                  description=expense_input.get('description'))
+                expense.save()
 
     else:
         form = ExpenseForm()
 
-    return render(request, "data_tables.html", {'form': form, 'expenses': all_expenses, 'debts': all_debts})
+    return render(request, "data_tables.html", {'form': form, 'expenses': all_expenses, 'debts': all_debts, 'resident': request.user.resident})
 
 
 def resident_profile(request, resident_id):
