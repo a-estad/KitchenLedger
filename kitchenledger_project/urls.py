1280ab967923c82ff86f7b03da1b558cbb5f32d3
balo? <Bastian.preisel@hotmail.com>
2024-10-14 17:23:55 +0200

    Added startpage to empty path

--- kitchenledger_project/urls.py
+++ kitchenledger_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("", include("kitchenapp.urls")),
     path("kitchenapp/", include("kitchenapp.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/login/", auth_views.LoginView.as_view(template_name="login.html")),
