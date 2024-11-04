9dc94e0ad750cbdc689aa0063bf8964202e92ec8
Bastian <Bastian.preisel@hotmail.com>
2024-11-04 12:19:12 +0100

    signals.py cleaner code

--- kitchenapp/signals.py
+++ kitchenapp/signals.py
@@ -1,66 +1,54 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Expense, Resident, Credit, DinnerClub, DinnerClubParticipant
+from .models import Debt, Expense, Resident, Credit, DinnerClub, DinnerClubParticipant
 
 # Signal to update balance when an expense is created or updated
 @receiver(post_save, sender=Expense)
 def update_resident_balance_on_expense(sender, instance, created, **kwargs):
-    resident = instance.paid_by
-    residents = Resident.objects.all()
-
-    if instance.is_dinner_club:
-        # For dinner club expenses, get the specific participants
-        dinner_club = DinnerClub.objects.get(expense=instance)
-        dinner_club_participants = DinnerClubParticipant.objects.filter(dinner_club=dinner_club)
-        debtors = [participant.resident for participant in dinner_club_participants]
-    else:
-        # For non-dinner club expenses, all residents are debtors
-        debtors = residents
-
-    # Calculate the per-person share of the expense
-    share = instance.cost / len(debtors)
-
-    # Subtract the share from each debtor
-    for debtor in debtors:
-        debtor.balance -= share
-        debtor.save()
-
-    # Add back the resident's share, so they only pay their portion
-    resident.balance += instance.cost - share
-    resident.save()
-
-
-# Signal to update balance on credit creation
-# @receiver(post_save, sender=Credit)
-# def update_resident_balance_on_credit(sender, instance, created, **kwargs):
-#     resident = instance.resident
-#     resident.balance -= instance.amount  # Reduce balance by credit amount
-#     resident.save()
+    adjust_balances_for_expense(expense=instance, operation='add')
 
 # Signal to revert balance on expense deletion
 @receiver(post_delete, sender=Expense)
 def revert_balance_on_expense_delete(sender, instance, **kwargs):
-    resident = instance.paid_by
-    residents = Resident.objects.all()
+    adjust_balances_for_expense(expense=instance, operation='subtract')
 
-    if instance.is_dinner_club:
-        # For dinner club expenses, get the specific participants
-        dinner_club = DinnerClub.objects.get(expense=instance)
+def adjust_balances_for_expense(expense: Expense, operation: str):
+    """
+    Adjust the balances for residents based on an expense.
+    
+    Parameters:
+    - expense: The Expense instance being processed.
+    - operation: A string, either 'add' or 'subtract', indicating the balance adjustment.
+    """
+    residentThatPaid = expense.paid_by
+    residents = (
+        (Resident.objects.filter(move_out_date__gt=expense.date, move_in_date__lt=expense.date, ) |
+        Resident.objects.filter(move_in_date__isnull=True)) &
+        Resident.objects.filter(is_superuser=False)
+    ).distinct()
+
+    if expense.is_dinner_club:
+        dinner_club = DinnerClub.objects.get(expense=expense)
         dinner_club_participants = DinnerClubParticipant.objects.filter(dinner_club=dinner_club)
         debtors = [participant.resident for participant in dinner_club_participants]
     else:
-        # For non-dinner club expenses, all residents are debtors
-        debtors = residents
+        debtors = list(residents)
 
-    # Calculate the per-person share of the expense
-    share = instance.cost / len(debtors)
+    share = expense.cost / len(debtors)
+    debtors = [resident for resident in debtors if resident != residentThatPaid]
 
-    # Subtract the share from each debtor
     for debtor in debtors:
-        debtor.balance += share
+        if operation == 'add':
+            Debt.objects.create(resident=debtor, expense=expense, amount=share)
+            debtor.balance -= share
+        elif operation == 'subtract':
+            debtor.balance += share
         debtor.save()
 
-    # Add back the resident's share, so they only pay their portion
-    resident.balance -= instance.cost - share
-    resident.save()
+    if operation == 'add':
+        Debt.objects.create(resident=residentThatPaid, expense=expense, amount=share)
+        residentThatPaid.balance += expense.cost - share
+    elif operation == 'subtract':
+        residentThatPaid.balance -= expense.cost - share
+    residentThatPaid.save()
 
